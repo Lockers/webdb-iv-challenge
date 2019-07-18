60e6a92de93e350729b5032e54d5f9f9894679df
Matt Locklin <mattlocklin@live.co.uk>
2019-07-18 16:01:04 +0100

    FInished MVP and stretch goals for sql queries

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -9,7 +9,6 @@ findById = (id) => {
 }
 
 findSteps = (id) => {
-    console.log('I am Firing')
     return db('schemes').select(
         'schemes.scheme_name',
         'steps.step_number',
@@ -20,8 +19,31 @@ findSteps = (id) => {
         'schemes.id').where('schemes.id', id).orderBy('step_number')
     
 }
+
+addScheme = (newScheme) => {
+    console.log('I am firing')
+    return db('schemes').insert(newScheme)
+}
+
+update = (changes, id) => {
+    updated = changes.scheme_name
+    return db('schemes')
+        .where({ id })
+        .update({
+        scheme_name: updated
+    })
+}
+
+remove = (id) => {
+    return db('schemes')
+        .where({ id })
+        .del()
+    }
 module.exports = {
     find,
     findById,
     findSteps,
+    addScheme,
+    update,
+    remove
 }
